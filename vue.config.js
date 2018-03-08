6a11310e995cd016bbc6d6efb89e24716790ee83
GimYoungPhil <youngphil.gim@gmail.com>
2018-03-09 01:31:08 +0900

    add vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+module.exports = {
+  devServer: {
+    historyApiFallback: true,
+    contentBase: [path.resolve(__dirname, './dist'), path.resolve(__dirname, './data')],
+    // proxy: {
+    //   "/api/*": {
+    //     target: "https://api.ktube.xyz",
+    //     secure: false,
+    //     changeOrigin: true
+    //   }
+    // },
+    host: '0.0.0.0',
+    port: 8080,
+  }
+}
