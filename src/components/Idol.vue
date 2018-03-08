7df63766624bad94627e85a24ca533858c0efb90
GimYoungPhil <youngphil.gim@gmail.com>
2018-03-09 01:32:31 +0900

    fixed Idol.vue

--- src/components/Idol.vue
+++ src/components/Idol.vue
@@ -1,10 +1,36 @@
 <template>
-  <h1>Idol Pages</h1>
+  <div>
+    <h1>Idol Pages</h1>
+    <div>
+      <h1>{{ idolDetail.detail.name }}</h1>
+      <h6>
+        {{ idolDetail.detail.height && `${idolDetail.detail.height} cm` }}
+        {{ idolDetail.detail.weight && `, ${idolDetail.detail.weight} kg` }}
+      </h6>
+      <img :src="`/images/${idolDetail.detail.id}.jpeg`" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import { mapState, mapActions } from 'vuex'
 
+export default {
+  name: 'Idol',
+  computed: {
+    ...mapState([
+      'idolDetail'
+    ]),
+  },
+  methods: {
+    ...mapActions([
+      'fetchIdolDetail',
+    ]),
+  },
+  mounted() {
+    const { idolKey } = this.$route.params
+    this.fetchIdolDetail(idolKey)
+  },
 }
 </script>
 
