5274e147d28d1a9bfcbe9e780ded8365127d30dc
GimYoungPhil <youngphil.gim@gmail.com>
2018-03-09 14:41:37 +0900

    fixed api

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,12 @@
 import axios from 'axios'
 
 export default {
+  getGirlGroups() {
+    return axios({
+      url: '/api/idols/girlgroups.json',
+    }).then(response => response.data)
+  },
+
   getIdolDetail({ idolKey }) {
     return axios({
       url: `/api/idols/${idolKey}.json`
